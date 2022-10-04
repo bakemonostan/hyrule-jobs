052cbf6fad23289901576907ab7a87cf14838dac
bakemonostan <ehizomokhomion@gmail.com>
2022-10-04 10:17:20 -0700

    created global styles and cleared default template

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app"><h3> Hello typescript vue </h3></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+});
+</script>
+
+<style>
+#app {
+}
+</style>
+
